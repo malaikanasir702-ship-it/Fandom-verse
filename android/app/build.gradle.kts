5f502da07c341fca70b43b5368e4fc95efe12d26
Malaika Nasir <malaikanasir702@gmail.com>
2026-09-25 18:51:58 +0500

    mnmnm

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,11 +10,12 @@ plugins {
 android {
     namespace = "com.fandomverse.fandom_verse"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -26,7 +27,7 @@ android {
         applicationId = "com.fandomverse.fandom_verse"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -43,4 +44,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
